4ce88b11e313f640f531911871cae206de0424d7
candychu17 <e0941678@u.nus.edu>
2022-06-23 20:52:56 +0800

    first commit

---
+++ testjune23/src/main/java/com/example/testjune23/Testjune23Application.java
@@ -0,0 +1,13 @@
+package com.example.testjune23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testjune23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testjune23Application.class, args);
+	}
+
+}
---
+++ testjune23/src/test/java/com/example/testjune23/Testjune23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testjune23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testjune23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
